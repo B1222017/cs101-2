49890cff94b8af39de0a739e7a2ed6cdd1d0c1d1
B1222017 <161683394+B1222017@users.noreply.github.com>
2025-04-10 15:33:15 +0800

    Update main25.c

--- EX4/main25.c
+++ EX4/main25.c
@@ -1,20 +1,12 @@
 #include <stdio.h>
 
-int multi(int i,int j){
-    if(j == 1){
-        printf("%d * %d = %d ",i,j,i*j);
-        return 0;
-    }else{
-        multi(i, j-1);
-        printf("%d * %d = %d ", i, j, i*j);
-        
-
-    }
+int my_strlen(char* s){
+    if(!*s)return 0;
+    return 1 + my_strlen(s+1);
 }
+
 int main(){
-    for(int i=1;i <= 9;i++){
-        multi(i,9);
-        printf("\n");
-    }
+    char s[] = "IU is a girl!";
+    printf("(%s)len = %d\n", s, my_strlen(s));
     return 0;
 }
